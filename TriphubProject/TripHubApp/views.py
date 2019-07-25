ca1e0db59b2c4b132e456ace9dacbb39dd476dc3
GoMotiv <newage3333@gmail.com>
2019-07-25 15:05:22 +0900

    장고와 리액트 연동

--- TriphubProject/TripHubApp/views.py
+++ TriphubProject/TripHubApp/views.py
@@ -1,39 +1,57 @@
 from django.shortcuts import render, redirect
 from django.utils import timezone
 from django.contrib.auth.models import User
-from .models import RoomInput   #데이터베이스 틀 임포트
-from .models import myRoom   #데이터베이스 틀 임포트
+from .models import RoomInput  # 데이터베이스 틀 임포트
+from .models import myRoom  # 데이터베이스 틀 임포트
 from django.contrib.auth.decorators import login_required
+
+# django + react api 연동
+from rest_framework import viewsets
+from .serializers import RoomInputSerializer, myRoomSerializer
 
 
 @login_required
 def main(request):
     user_name = request.user.username
-    user_name += '님 Halo!'
-    return render(request,'main.html',{'user_name':user_name})
-    
+    user_name += "님 Halo!"
+    return render(request, "main.html", {"user_name": user_name})
+
+
 def index(request):
 
     # triphub = TripHub
-    return render(request,'index.html' )
+    return render(request, "index.html")
 
-def createroom(request):    #입력받은 내용을 데이터베이스에 넣어주는 함수
+
+def createroom(request):  # 입력받은 내용을 데이터베이스에 넣어주는 함수
     room = RoomInput()
-    room.roomname = request.GET['roomname'] #방이름
-    room.date = timezone.datetime.now()  #날짜
-    room.mainmember = request.user.username #방장
-    room.save() #쿼리셋 메소드 중 하나. room에 넣은 내용을 저장해라는 함수 객체.delete()객체에 해당하는 내용을 데이터베이스 내용을 지워라
+    room.roomname = request.GET["roomname"]  # 방이름
+    room.date = timezone.datetime.now()  # 날짜
+    room.mainmember = request.user.username  # 방장
+    room.save()  # 쿼리셋 메소드 중 하나. room에 넣은 내용을 저장해라는 함수 객체.delete()객체에 해당하는 내용을 데이터베이스 내용을 지워라
 
-    #해당 그룹 방 테이블 생성
+    # 해당 그룹 방 테이블 생성
     myroom = myRoom()
     myroom.room_id = room.id
     myroom.name = room.mainmember
     myroom.save()
-    return redirect('Rooms/invite_member')
-    
+    return redirect("Rooms/invite_member")
+
+
 # def myroom(request):  #입력받은 내용을 화면에 띄워주는 함수.
 #     return render(request,'main.html')
 
-def info(request):
-    return render(request,'info.html')
 
+def info(request):
+    return render(request, "info.html")
+
+
+# django + react api 연동
+class RoomInputView(viewsets.ModelViewSet):
+    serializer_class = RoomInputSerializer
+    queryset = RoomInput.objects.all()
+
+
+class myRoomView(viewsets.ModelViewSet):
+    serializer_class = myRoomSerializer
+    queryset = myRoom.objects.all()
--- TriphubProject/TriphubProject/settings.py
+++ TriphubProject/TriphubProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3l=3l0^%n_u!ikb2xa$7gc%%7vcejo=2tk&w5fbt(!h_fx)3f)'
+SECRET_KEY = "3l=3l0^%n_u!ikb2xa$7gc%%7vcejo=2tk&w5fbt(!h_fx)3f)"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,46 +31,55 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'TripHubApp',
-    'accounts',
-    'Rooms',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "TripHubApp",
+    "accounts",
+    "Rooms",
+    # django + react 연동
+    "rest_framework",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",  # django + react 연동
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'TriphubProject.urls'
+CORS_ORIGIN_WHITELIST = ( 'http://localhost:3000', )
+
+ROOT_URLCONF = "TriphubProject.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            os.path.join(BASE_DIR, "templates"),
+            os.path.join(BASE_DIR, "frontend", "build"),
+        ],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ]
         },
-    },
+    }
 ]
 
-WSGI_APPLICATION = 'TriphubProject.wsgi.application'
+WSGI_APPLICATION = "TriphubProject.wsgi.application"
 
 
 # Database
@@ -99,16 +108,17 @@ WSGI_APPLICATION = 'TriphubProject.wsgi.application'
 # }
 
 import pymysql
+
 pymysql.install_as_MySQLdb()
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'triphub',
-        'USER': 'root',
-        'PASSWORD': 'asd489456+',
-        'HOST': '15.164.103.198',
-        'PORT': '3306',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "triphub",
+        "USER": "root",
+        "PASSWORD": "asd489456+",
+        "HOST": "15.164.103.198",
+        "PORT": "3306",
     }
 }
 
@@ -118,26 +128,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
     },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'ko-kr'
+LANGUAGE_CODE = "ko-kr"
 
-TIME_ZONE = 'Asia/Seoul'
+TIME_ZONE = "Asia/Seoul"
 
 USE_I18N = True
 
@@ -149,20 +153,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'), ]
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "frontend", "build"),
+]
 
 
-LOGIN_REDIRECT_URL = '/main'
-LOGOUT_REDIRECT_URL = '/accounts/login'
+LOGIN_REDIRECT_URL = "/main"
+LOGOUT_REDIRECT_URL = "/accounts/login"
 
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = "smtp.gmail.com"
 # 메일을 호스트하는 서버
-EMAIL_PORT = '587'
+EMAIL_PORT = "587"
 # gmail과의 통신하는 포트
-EMAIL_HOST_USER = 'gt03051@gmail.com'
+EMAIL_HOST_USER = "gt03051@gmail.com"
 # 발신할 이메일
-EMAIL_HOST_PASSWORD = '16331541z'
+EMAIL_HOST_PASSWORD = "16331541z"
 # 발신할 메일의 비밀번호
 EMAIL_USE_TLS = True
 # TLS 보안 방법
